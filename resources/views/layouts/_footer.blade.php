b82dcb5daf1adf3c72ede3d581f9a152ed83bf05
ZhangJiawei <zhangjiawei.cn@Hotmail.com>
2019-07-22 10:01:51 +0800

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="http://weibo.com/u/2762619275?profile_ftype=1" target="_blank">Leyi422</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:zhangjiawei.cn@hotmail.com">联系我们</a></p>
+    </div>
+</footer>
