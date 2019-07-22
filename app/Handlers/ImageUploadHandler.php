88dbd6a01259af6c2421a1cb5cb7ab12379d11d3
ZhangJiawei <zhangjiawei.cn@Hotmail.com>
2019-07-22 17:38:06 +0800

    授权访问

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,13 +2,13 @@
 
 namespace App\Handlers;
 
-use Illuminate\Support\Str;
+use Image;
 
 class ImageUploadHandler
 {
     protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
@@ -16,7 +16,7 @@ class ImageUploadHandler
 
         $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
 
-        $filename = $file_prefix . '_' . time() . '_' . Str::random(10) . '.' . $extension;
+        $filename = $file_prefix . '_' . time() . '_' . str_random(10) . '.' . $extension;
 
         if (! in_array($extension, $this->allowed_ext)) {
             return false;
@@ -24,8 +24,24 @@ class ImageUploadHandler
 
         $file->move($upload_path, $filename);
 
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        $image->save();
     }
 }
